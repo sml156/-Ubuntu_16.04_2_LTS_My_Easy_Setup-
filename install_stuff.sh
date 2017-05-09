2e284a69561ea08d8a9d7d4e6d6814ef8ddf31ff
sml156 <sml156@hotmail.com>
2017-05-09 18:06:08 -0400

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -3,10 +3,10 @@
 
 # Upgrade installed packages
 echo "\nUpgrade everything even the kernel and clean up some crap...\n"
-sudo apt -y update 
-sudo apt -y upgrade 
-sudo apt -y dist-upgrade 
-sudo apt -y autoremove
+#sudo apt -y update 
+#sudo apt -y upgrade 
+#sudo apt -y dist-upgrade 
+#sudo apt -y autoremove
 
 
 #Keep adding stuff here
