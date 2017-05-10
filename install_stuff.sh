885c482ad5bac43291d220d24995a214af093d44
sml156 <sml156@hotmail.com>
2017-05-09 22:39:36 -0400

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # 
 echo "\nThis script will take quite a while, Go make a coffie\n"
-# Upgrade installed packages
+# Upgrade installed packages Quietly 
 echo "\nUpgrade everything even the kernel and clean up some crap...\n"
-#sudo apt -y update 
-#sudo apt -y upgrade 
-#sudo apt -y dist-upgrade 
-#sudo apt -y autoremove
+sudo apt -qq update && sudo apt -qq dist-upgrade
 
 
 #Keep adding stuff here
-sudo apt -y install nano
-sudo apt -y install rcconf sysv-rc-conf
+echo "\nInstall nano...\n \a "
+sudo apt -qq install nano
+echo "\nInstall rcconf and sysv-rc-conf...\n \a "
+sudo apt -qq install rcconf sysv-rc-conf
 
 echo "\nDone...\n \a "
+
+
+# sudo dpkg-reconfigure tzdata
