279f8ed16cff9f0139d3b9d0a469b2c59d570130
sml156 <sml156@hotmail.com>
2017-05-11 07:12:47 -0400

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # 
+# -Ubuntu_16.04_2_LTS_My_Easy_Setup-
+#https://github.com/sml156/-Ubuntu_16.04_2_LTS_My_Easy_Setup-/blob/master/install_stuff.sh
+#
+
 echo "\nThis script will take quite a while, Go make a coffie\n"
 # Upgrade installed packages Quietly 
 echo "\nUpgrade everything even the kernel and clean up some crap...\n"
-sudo apt -qq update && sudo apt -qq dist-upgrade
+apt -qq update && apt -qq dist-upgrade
 
 
 #Keep adding stuff here
 echo " "
 echo "$(tput setaf 0) $(tput setab 7)...Install Nano...$(tput sgr 0)"
-sudo apt -qq install nano
+apt -qq install nano
 echo " "
 echo "$(tput setaf 0) $(tput setab 7)...Install rcconf and sysv-rc-conf...$(tput sgr 0)"
-sudo apt -qq install rcconf sysv-rc-conf
+apt -qq install rcconf sysv-rc-conf
 
 echo " "
 echo "$(tput setaf 0) $(tput setab 7)...Done...$(tput sgr 0)"
-
-# sudo dpkg-reconfigure tzdata
