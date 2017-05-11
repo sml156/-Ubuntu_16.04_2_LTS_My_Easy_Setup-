5445c81907af76f4f6c473d8111b5792c46eaf97
sml156 <sml156@hotmail.com>
2017-05-11 09:18:17 -0400

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -34,10 +34,6 @@ echo "$(tput setaf 0) $(tput setab 7)                                           
 echo "$(tput setaf 0) $(tput setab 7)                   ...Done...                                 $(tput sgr 0)"
 echo "$(tput setaf 0) $(tput setab 7)                                                              $(tput sgr 0)"
 echo " "
-
-
-
-
-
-
-sudo apt-get install iptraf
+echo " "
+echo " "
+echo " "
