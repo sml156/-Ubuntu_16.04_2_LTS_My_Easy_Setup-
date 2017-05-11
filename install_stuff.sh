f5408b364d51ae5787f79266d3e2ba997b90e1c8
sml156 <sml156@hotmail.com>
2017-05-11 07:14:56 -0400

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -7,16 +7,16 @@
 echo "\nThis script will take quite a while, Go make a coffie\n"
 # Upgrade installed packages Quietly 
 echo "\nUpgrade everything even the kernel and clean up some crap...\n"
-apt -qq update && apt -qq dist-upgrade
+sudo apt -qq update && sudo apt -qq dist-upgrade
 
 
 #Keep adding stuff here
 echo " "
 echo "$(tput setaf 0) $(tput setab 7)...Install Nano...$(tput sgr 0)"
-apt -qq install nano
+sudo apt -qq install nano
 echo " "
 echo "$(tput setaf 0) $(tput setab 7)...Install rcconf and sysv-rc-conf...$(tput sgr 0)"
-apt -qq install rcconf sysv-rc-conf
+sudo apt -qq install rcconf sysv-rc-conf
 
 echo " "
 echo "$(tput setaf 0) $(tput setab 7)...Done...$(tput sgr 0)"
